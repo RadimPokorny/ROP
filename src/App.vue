d2c4af696576575b10979fbe7e98ebc37e1b4219
kingradas <pokornyr.04@spst.eu>
2023-06-22 22:30:16 +0200

    elementary components add

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,128 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div class="center">
-    <h1>Hello world</h1>
-    <p class="">This is my new page</p>
-    <div class="container">
-      <h1 class="">Click here</h1>
+  
+  <div class="content">
+
+    <div class="center">
+
+
+      <div class="column">
+
+        <div class="component">
+
+          <Dropdown v-model="selectedCity" :options="groupedCities" optionLabel="label" optionGroupLabel="label" optionGroupChildren="items" placeholder="Select" class="w-full md:w-14rem">
+
+            <template #optiongroup="slotProps">
+                <div class="flex align-items-center">
+                    <img :alt="slotProps.option.label" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
+                    <div>{{ slotProps.option.label }}</div>
+                </div>
+            </template>
+
+          </Dropdown>
+
+        </div>
+
+      
+        <div class="component">
+
+          <Textarea v-model="value" autoResize rows="5" cols="30" />
+
+          <div class="button">
+
+            <Button icon="pi pi-copy" aria-label="Filter" />
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="column">
+
+        <Button icon="pi pi-arrow-right-arrow-left" aria-label="Filter" />
+
+        <div class="button-submit">
+
+          <Button label="Submit" />
+
+        </div>
+
+
+      </div>
+
+      
+      
+      
+      <div class="column">
+
+        <div class="component">
+
+          <Dropdown v-model="selectedCity" :options="groupedCities" optionLabel="label" optionGroupLabel="label" optionGroupChildren="items" placeholder="Select" class="w-full md:w-14rem">
+
+            <template #optiongroup="slotProps">
+                <div class="flex align-items-center">
+                    <img :alt="slotProps.option.label" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
+                    <div>{{ slotProps.option.label }}</div>
+                </div>
+            </template>
+
+          </Dropdown>
+
+        </div>
+
+      
+        <div class="component">
+
+          <Textarea v-model="value" autoResize rows="5" cols="30" />
+
+          <div class="button">
+
+            <Button icon="pi pi-copy" aria-label="Filter" />
+
+          </div>
+
+
+        </div>
+
+      </div>
+      
+      
+
+
     </div>
+
   </div>
   
 
 </template>
 
 <style scoped>
+.button{
+  margin-left: 50px;
+  margin-right: 50px;
+  margin-top: 20px;
+}
+
+.button-submit{
+  margin-top: 25px;
+}
+.content{
+  position: absolute !important;
+
+  width: auto !important;
+  height: auto !important;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
 
 .container{
   display: flex;
@@ -26,6 +133,25 @@ import HelloWorld from './components/HelloWorld.vue'
   width: 300px;
   height: 80px;
   border-radius: 20px;
+}
+
+.component{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: auto;
+  height: auto;
+  margin: 10px;
+}
+
+.column{
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 10px;
 }
 
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,31 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+
 import router from './router'
+
+//theme
+import "primevue/resources/themes/lara-light-indigo/theme.css"
+    
+//core
+import "primevue/resources/primevue.min.css"
+
+//icons
+import 'primeicons/primeicons.css'
+
+import Textarea from 'primevue/textarea'
+
+import Dropdown from 'primevue/dropdown'
+
+import Button from 'primevue/button';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(PrimeVue)
+app.component('Textarea', Textarea)
+app.component('Dropdown', Dropdown)
+app.component('Button', Button)
 app.mount('#app')
