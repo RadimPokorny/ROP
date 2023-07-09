fe4e30238a608419bbd8c426841d4784f46af14b
kingradas <pokornyr.04@spst.eu>
2023-07-09 14:06:21 +0200

    Add copy animation, one swap button, output readonly, dropdown search

--- src/App.vue
+++ src/App.vue
@@ -19,29 +19,6 @@ const groupedTypes = ref([
             { label: 'Dec', value: 'Dec' },
             { label: 'Bin', value: 'Bin' }
         ]
-    },
-    {
-        label: 'Hashing',
-        code: 'ha',
-        items: [
-            { label: 'MD', value: 'MD' },
-            { label: 'SHA', value: 'SHA' },
-            { label: 'LM', value: 'LM' },
-            { label: 'NT', value: 'NT' },
-            { label: 'CRC', value: 'CRC' },
-            { label: 'Bcrypt', value: 'Bcrypt' }, 
-            { label: 'Argon2', value: 'Argon2' }
-        ]
-    },
-    {
-        label: 'Encrypting',
-        code: 'en',
-        items: [
-            { label: 'AES', value: 'AES' },
-            { label: 'RC', value: 'RC' },
-            { label: 'RSA', value: 'RSA' },
-            { label: 'DES', value: 'DES' }
-        ]
     }
 ]);
 
@@ -64,13 +41,18 @@ function swapValue(): void {
 
 }
 
+const isCopied = ref(false);
+const isCopied2 = ref(false);
+
 function copyValue(): void {
   if (value.value) {
     navigator.clipboard.writeText(value.value);
-    alert("Value copied to clipboard.");
-  } 
-  
-  else {
+    isCopied.value = true; // Set the isCopied flag to true after successful copying
+
+    setTimeout(() => {
+      isCopied.value = false; // Reset the isCopied flag after 1 second
+    }, 1000);
+  } else {
     alert("No input text.");
   }
 }
@@ -78,11 +60,12 @@ function copyValue(): void {
 function copyValue2(): void {
   if (value2.value) {
     navigator.clipboard.writeText(value2.value);
-    alert("Value copied to clipboard.");
-  } 
+    isCopied2.value = true; // Set the isCopied flag to true after successful copying
 
-  else 
-  {
+    setTimeout(() => {
+      isCopied2.value = false; // Reset the isCopied flag after 1 second
+    }, 1000);
+  } else {
     alert("No input text.");
   }
 }
@@ -105,7 +88,7 @@ function isBoldGroup(code: string): boolean {
 <template>
 
   <header class="header">
-    <h1 class="title">Web interface for penetration testing</h1>
+    <p class="title">Web interface for penetration testing</p>
   </header>
   
   <div class="content">
@@ -115,9 +98,9 @@ function isBoldGroup(code: string): boolean {
 
       <div class="column">
 
-        <div class="component">
+        <div class="dropdown-copy">
 
-          <Dropdown v-model="selectedType" :options="groupedTypes" optionLabel="label" optionGroupLabel="label" optionGroupChildren="items" placeholder="Select" class="w-full md:w-14rem">
+          <Dropdown v-model="selectedType" :options="groupedTypes" filter optionLabel="label" optionGroupLabel="label" optionGroupChildren="items" placeholder="Select" class="w-full md:w-14rem">
 
             <template #optiongroup="slotProps">
               <div class="flex align-items-center">
@@ -126,6 +109,14 @@ function isBoldGroup(code: string): boolean {
             </template>
 
           </Dropdown>
+
+          <div class="button">
+            <Button
+              :icon="isCopied ? 'pi pi-check' : 'pi pi-copy'"
+              aria-label="Filter"
+              @click="copyValue"
+            />
+          </div>
 
         </div>
 
@@ -137,26 +128,17 @@ function isBoldGroup(code: string): boolean {
             <label>Input</label>
             </span>
 
-          <div class="button">
-
-            <Button icon="pi pi-copy" aria-label="Filter" @click="copyValue"/>
-
-          </div>
-
         </div>
 
       </div>
 
       <div class="column">
 
-        <Button icon="pi pi-arrow-left" aria-label="Filter" @click="swapValue"/>
-
-        <div class="button-submit">
-
-          <Button icon="pi pi-arrow-right" @click="handleClick"/>
-
-        </div>
-
+        <Button 
+          icon="pi pi-arrow-right-arrow-left" 
+          aria-label="Filter" 
+          @click="swapValue"
+        />
 
       </div>
 
@@ -165,9 +147,18 @@ function isBoldGroup(code: string): boolean {
       
       <div class="column">
 
-        <div class="component">
+        <div class="dropdown-copy">
 
-          <Dropdown v-model="selectedType2" :options="groupedTypes" optionLabel="label" optionGroupLabel="label" optionGroupChildren="items" placeholder="Select" class="w-full md:w-14rem">
+          <Dropdown 
+            v-model="selectedType2" 
+            :options="groupedTypes" 
+            filter
+            optionLabel="label" 
+            optionGroupLabel="label" 
+            optionGroupChildren="items" 
+            placeholder="Select" 
+            class="w-full md:w-14rem"
+          >
 
             <template #optiongroup="slotProps">
               <div class="flex align-items-center">
@@ -177,22 +168,29 @@ function isBoldGroup(code: string): boolean {
 
           </Dropdown>
 
+          <div class="button">
+            <Button
+              :icon="isCopied2 ? 'pi pi-check' : 'pi pi-copy'"
+              aria-label="Filter"
+              @click="copyValue2"
+            />
+          </div>
+
         </div>
 
       
         <div class="component">
 
           <span class="p-float-label">
-          <Textarea v-model="value2" autoResize rows="5" cols="30" />
+          <Textarea 
+            v-model="value2" 
+            autoResize rows="5" 
+            cols="30" 
+            disabled 
+          />
+
           <label>Output</label>
           </span>
-
-          <div class="button">
-
-            <Button icon="pi pi-copy" aria-label="Filter" @click="copyValue2"/>
-
-          </div>
-
 
         </div>
 
@@ -211,56 +209,60 @@ function isBoldGroup(code: string): boolean {
 
 
 
-<style scoped>
-.button{
-  margin-left: 50px;
-  margin-right: 50px;
-  margin-top: 20px;
+<style scoped lang="scss">  
+$color_1: white;
+$background-color_1: #f16736;
+
+.button {
+	margin: 20px;
 }
-
-.button-submit{
-  margin-top: 25px;
+.button-submit {
+	margin-top: 25px;
 }
-.content{
-  position: absolute !important;
-
-  width: auto !important;
-  height: auto !important;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+.content {
+	position: absolute !important;
+	width: auto !important;
+	height: auto !important;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
 }
-
-.container{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: hsla(160, 100%, 37%, 1);
-  color: white;
-  width: 300px;
-  height: 80px;
-  border-radius: 20px;
+.container {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: $background-color_1;
+	color: $color_1;
+	width: 300px;
+	height: 80px;
+	border-radius: 20px;
 }
-
-.component{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  width: auto;
-  height: auto;
-  margin: 10px;
+.component {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	width: auto;
+	height: auto;
+	margin: 10px;
 }
-
-.column{
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  margin: 10px;
+.dropdown-copy {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: row;
+	width: auto;
+	height: auto;
+	margin: 10px;
+}
+.column {
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	margin: 10px;
 }
 
 </style>
