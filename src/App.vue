485e1af5b1ae423845dac8c24b99672f414538e6
kingradas <pokornyr.04@spst.eu>
2023-07-15 13:05:12 +0200

    Add reset button, Remove spaces

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-
 import { ref } from "vue";
-
-
 const selectedType = ref();
 const selectedType2 = ref();
 const groupedTypes = ref([
@@ -21,10 +18,8 @@ const groupedTypes = ref([
         ]
     }
 ]);
-
 const value = ref("");
 const value2 = ref("");
-
 const handleClick = (): void => {
   if (value.value) {
     value2.value = value.value;
@@ -32,23 +27,17 @@ const handleClick = (): void => {
     alert("No input text.");
   }
 };
-
 function swapValue(): void {
   const third = value.value;
   value.value = value2.value;
   value2.value = third;
-
-
 }
-
 const isCopied = ref(false);
 const isCopied2 = ref(false);
-
 function copyValue(): void {
   if (value.value) {
     navigator.clipboard.writeText(value.value);
     isCopied.value = true; // Set the isCopied flag to true after successful copying
-
     setTimeout(() => {
       isCopied.value = false; // Reset the isCopied flag after 1 second
     }, 1000);
@@ -56,12 +45,10 @@ function copyValue(): void {
     alert("No input text.");
   }
 }
-
 function copyValue2(): void {
   if (value2.value) {
     navigator.clipboard.writeText(value2.value);
     isCopied2.value = true; // Set the isCopied flag to true after successful copying
-
     setTimeout(() => {
       isCopied2.value = false; // Reset the isCopied flag after 1 second
     }, 1000);
@@ -69,47 +56,40 @@ function copyValue2(): void {
     alert("No input text.");
   }
 }
-
 function isBoldGroup(code: string): boolean {
   if(code == 'ha' || code == 'en' || code == 'co'){
     return true;
   }
   return false;
 }
-
-
+function resetComps(): void {
+  value.value =  "";
+  value2.value = "";
+}
 </script>
-
-
-
-
-
-
 <template>
-
   <header class="header">
     <p class="title">Web interface for penetration testing</p>
   </header>
-  
   <div class="content">
-
     <div class="center">
-
-
       <div class="column">
-
         <div class="dropdown-copy">
-
-          <Dropdown v-model="selectedType" :options="groupedTypes" filter optionLabel="label" optionGroupLabel="label" optionGroupChildren="items" placeholder="Select" class="w-full md:w-14rem">
-
+          <Dropdown 
+            v-model="selectedType" 
+            :options="groupedTypes" 
+            filter 
+            optionLabel="label" 
+            optionGroupLabel="label" 
+            optionGroupChildren="items" 
+            placeholder="Select" 
+            style="width: 200px;">
             <template #optiongroup="slotProps">
               <div class="flex align-items-center">
                   <div>{{ slotProps.option.label }}</div>
               </div>
             </template>
-
           </Dropdown>
-
           <div class="button">
             <Button
               :icon="isCopied ? 'pi pi-check' : 'pi pi-copy'"
@@ -117,38 +97,35 @@ function isBoldGroup(code: string): boolean {
               @click="copyValue"
             />
           </div>
-
         </div>
-
-      
         <div class="component">
-
           <span class="p-float-label">
-            <Textarea v-model="value" autoResize rows="5" cols="30" />
+            <Textarea 
+              v-model="value" 
+              autoResize 
+              rows="7" 
+              cols="50" 
+            />
             <label>Input</label>
             </span>
-
         </div>
-
       </div>
-
       <div class="column">
-
         <Button 
           icon="pi pi-arrow-right-arrow-left" 
           aria-label="Filter" 
           @click="swapValue"
+          class="swap-btn"
         />
-
+        <Button 
+          aria-label="Reset" 
+          label="Reset"
+          @click="resetComps"
+          class="reset-btn"
+        />
       </div>
-
-      
-      
-      
       <div class="column">
-
         <div class="dropdown-copy">
-
           <Dropdown 
             v-model="selectedType2" 
             :options="groupedTypes" 
@@ -157,17 +134,13 @@ function isBoldGroup(code: string): boolean {
             optionGroupLabel="label" 
             optionGroupChildren="items" 
             placeholder="Select" 
-            class="w-full md:w-14rem"
-          >
-
+            style="width: 200px;">
             <template #optiongroup="slotProps">
               <div class="flex align-items-center">
                   <div>{{ slotProps.option.label }}</div>
               </div>
             </template>
-
           </Dropdown>
-
           <div class="button">
             <Button
               :icon="isCopied2 ? 'pi pi-check' : 'pi pi-copy'"
@@ -175,44 +148,26 @@ function isBoldGroup(code: string): boolean {
               @click="copyValue2"
             />
           </div>
-
         </div>
-
-      
         <div class="component">
-
           <span class="p-float-label">
           <Textarea 
             v-model="value2" 
-            autoResize rows="5" 
-            cols="30" 
+            autoResize 
+            rows="7" 
+            cols="50" 
             disabled 
           />
-
           <label>Output</label>
           </span>
-
         </div>
-
       </div>
-      
-      
-
-
     </div>
-
   </div>
-  
-
 </template>
-
-
-
-
 <style scoped lang="scss">  
 $color_1: white;
 $background-color_1: #f16736;
-
 .button {
 	margin: 20px;
 }
@@ -264,5 +219,10 @@ $background-color_1: #f16736;
 	flex-direction: column;
 	margin: 10px;
 }
-
+.reset-btn{
+  margin-top: 50px;
+}
+.swap-btn{
+  margin-top: 65px;
+}
 </style>
