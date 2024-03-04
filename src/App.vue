d373fcf6934238cde23ee80615729e1ed2b7af99
radimpokorny <pokornyr.04@spst.eu>
2024-03-04 17:49:11 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import {decode} from 'html-entities';
 import { saveAs } from 'file-saver';
 import { Buffer } from 'buffer';
 
-window.Buffer = Buffer;
+window.Buffer = window.Buffer || Buffer;
 
 //Hash methods
 import {Md5} from 'ts-md5';
