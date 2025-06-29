81e033e695690d457c0a5c749d4bf1c1bbb0b962
RadimPokorny19 <98946296+RadimPokorny@users.noreply.github.com>
2025-06-29 16:06:13 +0200

    Fix build entry point

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.js'),
+      entry: resolve(__dirname, 'src/main.ts'),
       name: 'MyLib',
       // the proper extensions will be added
       fileName: 'my-lib',
